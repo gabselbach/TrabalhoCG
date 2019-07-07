c6961f8ed17bec28906066b1ed924eb18898a4f3
Gabriella Selbach <gabriellaselbach@gmail.com>
2019-07-06 21:00:45 -0300

    falta pouco

--- src/CG_UFPel/main.cpp
+++ src/CG_UFPel/main.cpp
@@ -1,3 +1,6 @@
+#if defined(_MSC_VER) && (_MSC_VER >= 1310)
+#   pragma warning(disable: 4996) // Disable the fopen, strcpy, sprintf being unsafe warning
+#endif
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -9,21 +12,26 @@
 #include <learnopengl/shader_m.h>
 #include <learnopengl/camera.h>
 #include <learnopengl/model.h>
-
+#include <learnopengl/game.h>
+#include <irrklang/irrKlang.h>
 #include <iostream>
 #include <stdio.h>  
 #include <stdlib.h>  
 #include <time.h>  
 #include <math.h>  
+#include<string>
+using std::string;
+#define GLT_IMPLEMENTATION
+#include "gltext.h" /* https://github.com/MrVallentin/glText */
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
-
+    
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-
 // camera
 Camera camera(glm::vec3(0.0f, 1.5f, 10.0f));
 float lastX = SCR_WIDTH / 2.0f;
@@ -37,6 +45,8 @@ float lastFrame = 0.0f;
 float positionx1 = 0.0f, positionx2 = 0.0f,deltaT =0.0f;
 int modelCurrent = 5;
 bool animacao = false;
+bool perdeu = false;
+bool ganhou = false;
 float anda=0.0f;
 float sleep = 10.0f; 
 float altura_objetos = 5.8f;
@@ -47,11 +57,14 @@ float first_t_phone=0.0f;
 float first_t_book=0.0f;
  float min_x = -4.5f;
   float max_x =  4.3f;
+  float yPersonagem =0.8f;
+  int qual_model =1;
   //divide um pelo outro
   //pega a parte fracionaria e  multiplica pelo divisior
+  Game game(5);
 int main()
 {
-    
+  
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
@@ -63,8 +76,23 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
 #endif
 
+    // para o texto
+    GLFWmonitor *monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode *mode = glfwGetVideoMode(monitor);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+    glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+    glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+
+    glfwWindowHint(GLFW_ALPHA_BITS, 8);
+    glfwWindowHint(GLFW_DEPTH_BITS, 24);
+
     // glfw window creation
     // --------------------
+    
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -80,18 +108,23 @@ int main()
 
     // tell GLFW to capture our mouse
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSwapInterval(1);
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
+         fprintf(stderr, "Failed to load OpenGL functions and extensions\n");
+        glfwTerminate();
+        return EXIT_FAILURE;
     }
 
     // configure global opengl state
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
+
+   
+    // ATE AQUI
 
     // build and compile shaders
     // -------------------------
@@ -102,8 +135,12 @@ int main()
     Model ourModel1(FileSystem::getPath("resources/objects/nanosuit/nanosuit.obj"));
    Model ourModel2(FileSystem::getPath("resources/objects/mo/SS4_Zoom.obj"));
     Model ourModel3(FileSystem::getPath("resources/objects/book2/bookVintage.obj"));
+    Model ourModel4(FileSystem::getPath("resources/objects/planet/planet.obj"));
+    Model ourModel5(FileSystem::getPath("resources/objects/cyborg/cyborg.obj"));
     vector <glm::mat4> books;
     vector <glm::mat4> phones;
+    int pontos=0;
+    int vidas = 3;
     int contador_tempo_caidos_books=0;
      int contador_tempo_caidos_phones=0;
     int atual_books=0;
@@ -123,37 +160,46 @@ int main()
      float temp_caindo_phones =0.0f;
      int valido_books=0;
      float temp_caindo_books =0.0f;
+     bool colision[num_phones];
+     bool colision_b[num_books];
+     float positionXModel =0.0f;
+     float positionXPhones[num_phones];
   srand(time(NULL)); 
     for (int i = 0; i < num_books; ++i)
     {
+         colision_b[i]=false;
         delta_books[i]=0.0f;
         first_books[i] =0.0f;
         glm::mat4 m;
-        res = min_x + rand() * (max_x - min_x) / RAND_MAX;
-        m = glm::translate(m, glm::vec3(res, altura_objetos, 0.0f)); 
+        float pos_x = min_x + rand() * (max_x - min_x) / RAND_MAX;
+        m = glm::translate(m, glm::vec3(pos_x, altura_objetos, 0.0f)); 
         m = glm::scale(m, glm::vec3(0.1f, 0.1f, 0.1f));  
       books.push_back(m);
     }
     for (int i = 0; i < num_phones; ++i)
     {
+     
+        colision[i]=false;
         delta_phones[i]=0.0f;
          first_phones[i] =0.0f;
       glm::mat4 m;
-        res = min_x + rand() * (max_x - min_x) / RAND_MAX;
-        m = glm::translate(m, glm::vec3(res, altura_objetos, 0.0f)); 
+        float pos_x = min_x + rand() * (max_x - min_x) / RAND_MAX;
+        m = glm::translate(m, glm::vec3(pos_x, altura_objetos, 0.0f)); 
+        positionXPhones[i]=pos_x;
         m = glm::scale(m, glm::vec3(0.1f, 0.1f, 0.1f));  
       phones.push_back(m);
     }
     // draw in wireframe
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     float firstTime =0.0f;
-    /* inicializando a semente do gerador aleatorio*/
- 
-  //res = min_x + rand() * (max_x - min_x) / RAND_MAX;
 
       glm::mat4 model1; 
         model1 = glm::translate(model1, glm::vec3(0.0f, -2.5f, 0.0f)); 
-        model1 = glm::scale(model1, glm::vec3(0.2f, 0.2f, 0.2f));  
+        model1 = glm::scale(model1, glm::vec3(0.2f, 0.2f, 0.2f));
+        //model2 = glm::translate(model2, glm::vec3(0.0f, -2.5f, 0.0f)); 
+       // model2= glm::scale(model2, glm::vec3(0.2f, 0.2f, 0.2f));
+       // modelaux = glm::translate(modelaux, glm::vec3(model1[3][0]+1.0f, -2.0f, 0.0f)); 
+        //modelaux = glm::scale(modelaux, glm::vec3(0.2f, 0.2f, 0.2f));  
         // std::cout <<"\n\n";
         // std::cout <<"Bem vindo ao jogo tal\n\n";
          //std::cout <<"Instruções de jogo:\n";
@@ -162,43 +208,93 @@ int main()
     float temp_caindo =0.0f;
     i=valido_phones;
     j=valido_books;
-     glViewport( 0.0f, 0.0f, SCR_WIDTH, SCR_HEIGHT ); // specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
-    glMatrixMode( GL_PROJECTION ); // projection matrix defines the properties of the camera that views the objects in the world coordinate frame. Here you typically set the zoom factor, aspect ratio and the near and far clipping planes
-    glLoadIdentity( ); // replace the current matrix with the identity matrix and starts us a fresh because matrix transforms such as glOrpho and glRotate cumulate, basically puts us at (0, 0, 0)
-    glOrtho( 0, SCR_WIDTH, 0, SCR_HEIGHT, 0, 1 ); // essentially set coordinate system
-    glMatrixMode( GL_MODELVIEW ); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
-    glLoadIdentity( ); // same as above comment
-    
-    GLfloat lineVertices[] =
+    // texto
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    if (!gltInit())
     {
-        200, 100, 0,
-        100, 300, 0
-    };
+        fprintf(stderr, "Failed to initialize glText\n");
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    GLTtext *text2 = gltCreateText();
+     GLTtext *text3 = gltCreateText();
+     
+     int viewportWidth, viewportHeight;
+     bool comeco = false;
+     bool play_jogo= false;
     while (!glfwWindowShouldClose(window))
     {
-
-         float currentFrame = glfwGetTime();
+        float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
+        if(glfwGetKey(window, GLFW_KEY_P) && !play_jogo){
+            play_jogo=true;
+        }
+        if(play_jogo){
+        if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS && qual_model==2 && currentFrame >0.5f && !comeco){
+            model1[0][0]=0.2f;
+             model1[1][1]=0.2f;
+             model1[2][2]=0.2f;
+            qual_model =1;
+        }
+        if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS && qual_model==1 && currentFrame >0.5f && !comeco){
+             model1[0][0]=0.8f;
+             model1[1][1]=0.8f;
+             model1[2][2]=0.8f;
+             //model1 = glm::scale(model1, glm::vec3(0.8f, 0.8f, 0.8f));
+            qual_model =2;
+        }
+    if(vidas==0)
+        perdeu = true;
+    if(pontos==10)
+        ganhou = true;
+    char *pt = game.setaTexto(pontos,"pontos=");
+    char *vd = game.setaTexto(vidas,"vidas=");
+    gltSetText(text2,pt );
+    gltSetText(text3,vd );
+
+        //texto
+         glfwGetFramebufferSize(window, &viewportWidth, &viewportHeight);
+         glViewport(0, 0, viewportWidth, viewportHeight);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+         gltBeginDraw();
+        gltColor(1.0f, 1.0f, 1.0f, 1.0f);
+        
+         
          if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS && play_cai_phone==false && play_cai_book==false){
+            comeco = true;
             play_cai_phone= true;
             play_cai_book= true;
             first_phones[0]=currentFrame;
             first_books[0]=currentFrame+0.2f;
          }
+         if(comeco && !play_cai_phone && !play_cai_book)
+                std::cout <<"Acabou as coisas\n\n";
+         if(!perdeu && !ganhou ){
+            gltDrawText2D(text2, 10.0f, 0.0f, 1.0f);
+            gltDrawText2D(text3, 10.0f, 30.0f, 1.0f);
          if(play_cai_phone){
             temp_caindo_phones+=deltaTime;
-            std::cout <<"tempo caido phones:"<< temp_caindo_phones <<"\n";
-            if(i <liberado_phones)
-            {
+            
+            if(i <liberado_phones){
                 
                 delta_phones[i] = currentFrame - first_phones[i];
                 if(delta_phones[i] >0.0f){
+                     float x_min = model1[3][0]-0.5f;
+                                float x_max = model1[3][0]+0.5f;
                     if(delta_phones[i]<duracao_caida){
                             float val_y = (delta_phones[i] * 0.03f);
                             phones[i][3][1] -= val_y;
+
+                            if(phones[i][3][1]<- 0.9f && colision[i]==false && (phones[i][3][0]+2.0f)>x_min && phones[i][3][0]<x_max){
+                                ++pontos;
+                                colision[i]=true;
+                            }
                     }else{
                         ++valido_phones;
+                        phones[i][3][1] = -30.0f;
                     }
                     if ((temp_caindo_phones>res) && (liberado_phones!= num_phones))  {
                        
@@ -208,32 +304,39 @@ int main()
                         if(liberado_phones!= num_phones){
                             ++liberado_phones;
                            
-                        }else{
-                            play_cai_phone = false;
-                            break;
                         }
+                        if(liberado_phones>num_phones)
+                             play_cai_phone = false;
                     }
                 }
                 ++i;
             }else{
                 i=valido_phones;
             }
+            if(valido_phones==num_phones && !ganhou)
+                perdeu=true;
         }
         if(play_cai_book){
+             
+            temp_caindo_books+=deltaTime;
             if(j <liberado_books)
             {
-               
                 delta_books[j] = currentFrame - first_books[j];
                 if(delta_books[j] >0.0f){
-                    if(delta_books[j]<duracao_caida){
-                       
-                            float val_t = (delta_books[j] * 0.03f);
-                            books[j][3][1] -= val_t;
+                    float x_min = model1[3][0]-0.2f;
+                    float x_max = model1[3][0]+0.2f;
+                    if(delta_books[j]<duracao_caida){  
+                        float val_t = (delta_books[j] * 0.03f);
+                        books[j][3][1] -= val_t;
+                        if(books[j][3][1]< -0.9f && colision_b[j]==false && (books[j][3][0]+1.0f)>x_min && books[j][3][0]<x_max){
+                                --vidas;
+                                colision_b[j]=true;
+                            }
                     }else{
                         ++valido_books;
+                        books[j][3][1] = -30.0f;
                     }
-                    if ((temp_caindo_phones>res_b) && (liberado_books!= num_books))  {
-                       
+                    if ((temp_caindo_books>res_b) && (liberado_books!= num_books))  {
                           res_b+=2.8f;
                         first_books[liberado_books] = currentFrame;
                         delta_books[liberado_books] =0.0f;
@@ -241,6 +344,7 @@ int main()
                             ++liberado_books;
                            
                         }else{
+                           
                             play_cai_book = false;
                             break;
                         }
@@ -251,68 +355,105 @@ int main()
                 j=valido_books;
             }
         }
-        
-            
-        
-        // per-frame time logic
-        
-        // --------------------
-       
-        // input
-        // -----
+    }else{
+         GLTtext *tfinal = gltCreateText();
+         if(ganhou)
+            gltSetText(tfinal,"Parabens voce esta formado" );
+        if(perdeu)
+            gltSetText(tfinal,"Voce ficara eternamente na faculdade" );
+         gltDrawText2DAligned(tfinal,
+            (GLfloat)(viewportWidth / 2),
+            (GLfloat)(viewportHeight / 2),
+            1.5f,
+            GLT_CENTER, GLT_CENTER);
+    }
+    gltEndDraw(); 
         processInput(window);
-
-        // render
-        // ------
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
-        // don't forget to enable shader before setting uniforms
         ourShader.use();
-
-        // view/projection transformations
-        //inicialização
-      
-        //começou a animaçao
        
      glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
         ourShader.setMat4("projection", projection);
         ourShader.setMat4("view", view);
+        if(!perdeu && !ganhou ){
         if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS){
             anda = sleep * deltaTime;
-            model1 = glm::translate(model1,glm::vec3(-anda,0.0f,0.0f));
+            positionXModel -=0.1f;
+            model1 = glm::translate(model1,glm::vec3(-0.1f,0.0f,0.0f));
         }
         if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS){
             anda = sleep * deltaTime;
-            model1 = glm::translate(model1,glm::vec3(anda,0.0f,0.0f));
+            positionXModel +=0.1f;
+            if((model1[3][0]+0.1)> min_x && (model1[3][0]+0.1)< max_x)
+                model1 = glm::translate(model1,glm::vec3(0.1f,0.0f,0.0f));
+        }
+        if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS){
+            float we=0;
+            //restart jogo
         }
         
-      
-        ourShader.setMat4("model", model1);
-        ourModel1.Draw(ourShader);
-        for (int i = 0; i < num_books; ++i)
-        {
-            ourShader.setMat4("model", books[i]);
-            ourModel3.Draw(ourShader);
+            ourShader.setMat4("model", model1);
+            if(qual_model==1)
+                ourModel1.Draw(ourShader);
+            if(qual_model==2)
+                ourModel5.Draw(ourShader);
+            for (int i = 0; i < num_books; ++i)
+            {
+                if(!(colision_b[i])){
+                    ourShader.setMat4("model", books[i]);
+                    ourModel2.Draw(ourShader);
+                }
+            }
+            for (int i = 0; i < num_phones; ++i)
+            {
+                if(!(colision[i])){
+                    ourShader.setMat4("model", phones[i]);
+                    ourModel3.Draw(ourShader);
+                }
+            }
         }
-        for (int i = 0; i < num_phones; ++i)
-        {
-            ourShader.setMat4("model", phones[i]);
-            ourModel2.Draw(ourShader);
-        }
-           glEnable( GL_LINE_SMOOTH );
-        glEnable( GL_LINE_STIPPLE );
-        glPushAttrib( GL_LINE_BIT );
-        glLineWidth( 10 );
-        glLineStipple( 1, 0x00FF );
-        glEnableClientState( GL_VERTEX_ARRAY );
-        glVertexPointer( 3, GL_FLOAT, 0, lineVertices );
-        glDrawArrays( GL_LINES, 0, 2 );
-        glDisableClientState( GL_VERTEX_ARRAY );
-        glPopAttrib( );
-        glDisable( GL_LINE_STIPPLE );
-        glDisable( GL_LINE_SMOOTH );
+      }else{
+        glfwGetFramebufferSize(window, &viewportWidth, &viewportHeight);
+         glViewport(0, 0, viewportWidth, viewportHeight);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        GLTtext *text4 = gltCreateText();
+        GLTtext *text5 = gltCreateText();
+        GLTtext *text6 = gltCreateText();
+        GLTtext *text7 = gltCreateText();
+        GLTtext *text8 = gltCreateText();
+        GLTtext *text9 = gltCreateText();
+        GLTtext *text10 = gltCreateText();
+        GLTtext *text11 = gltCreateText();
+         gltSetText(text4,"Instruções de Jogo:" );
+         gltSetText(text5,"1- Para entrar no jogo :P");
+         gltSetText(text6,"2- Voce pode trocar de personagem antes de começar o jogo aperte: 1 ou 2");
+          gltSetText(text7,"3 - para começar o jogo aperte: C");
+          gltSetText(text8,"4 - para movimentar o boneco aperte nas setas para o lado do teclado"); 
+         gltSetText(text9,"Objetivo"); 
+          gltSetText(text10,"Voce deve pegar 10 livros para se formar");
+         gltSetText(text11," voce só tem 3 vidas então só pode pegar ate duas cameras " );
+           gltBeginDraw();
+            gltColor(1.0f, 1.0f, 1.0f, 1.0f);
+            gltDrawText2D(text4, 10.0f, 0.0f, 1.0f);
+            gltDrawText2D(text5, 10.0f, 30.0f, 1.0f);
+            gltDrawText2D(text6, 10.0f, 60.0f, 1.0f);
+            gltDrawText2D(text7, 10.0f, 90.0f, 1.0f);
+            gltDrawText2D(text8, 10.0f, 120.0f, 1.0f);
+            gltDrawText2D(text9, 10.0f, 150.0f, 1.0f);
+            gltDrawText2D(text10, 10.0f, 180.0f, 1.0f);
+            gltDrawText2D(text11, 10.0f, 210.0f, 1.0f);
+            gltEndDraw(); 
+          processInput(window);
+        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+        ourShader.use();
+       
+     glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        ourShader.setMat4("projection", projection);
+        ourShader.setMat4("view", view);
+      }
+
          glfwSwapBuffers(window);
         glfwPollEvents();
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
